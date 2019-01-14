fcdd971e78f46a9b193a1589dfe0644ee30dccce
googege <googege@icloud.com>
2019-01-15 00:30:30 +0800

    add

--- salt.go
+++ salt.go
@@ -1,4 +1,23 @@
 //salt.go This file is all about encrypting and decrypting passwords.
+//
+//func main(t *testing.T) {
+
+//	pass,salt := EncryptMd5("991182@")
+//	passReal := "991182@"
+//	a,b := DeEncryptMd5(passReal,pass,salt)
+//	if b != nil || !a {
+//		t.Error("❌")
+//	}
+//	fmt.Println(pass,salt)
+//	passErr := "123456!@"
+//	d,e := DeEncryptMd5(passErr,pass,salt)
+//	if e != nil || !d {
+//		t.Error("这个错误 应该被打印出来")
+//	}
+//
+//
+//}
+
 package goo
 
 import (
