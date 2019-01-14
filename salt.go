452c88d27588f5e666764a05c68811ea4e47930b
googege <googege@icloud.com>
2019-01-15 00:36:16 +0800

    Update salt.go

--- salt.go
+++ salt.go
@@ -1,21 +1,23 @@
-//salt.go This file is all about encrypting and decrypting passwords.
-//this is a example:
-//	pass,salt := EncryptMd5("991182@")
-//	passReal := "991182@"
-//	a,b := DeEncryptMd5(passReal,pass,salt)
-//	if b != nil || !a {
-//		t.Error("❌")
-//	}
-//	fmt.Println(pass,salt)
-//	passErr := "123456!@"
-//	d,e := DeEncryptMd5(passErr,pass,salt)
-//	if e != nil || !d {
-//		t.Error("这个错误 应该被打印出来")
-//	}
-//
-//
-//
+/*
+salt.go This file is all about encrypting and decrypting passwords.
+this is a example for you:
 
+	pass,salt := EncryptMd5("991182@")
+	passReal := "991182@"
+	a,b := DeEncryptMd5(passReal,pass,salt)
+	if b != nil || !a {
+		t.Error("❌")
+	}
+	fmt.Println(pass,salt)
+	passErr := "123456!@"
+	d,e := DeEncryptMd5(passErr,pass,salt)
+	if e != nil || !d {
+		t.Error("这个错误 应该被打印出来")
+	}
+
+
+
+*/
 package goo
 
 import (
